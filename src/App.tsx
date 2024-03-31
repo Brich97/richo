04bbe4aa4a34e42b4660fb6c3594d8d1bd0c680b
Brittany R <brittanyrichards135@gmail.com>
2024-03-31 11:15:15 +1000

    Adjust image

--- src/App.tsx
+++ src/App.tsx
@@ -18,16 +18,10 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={user.imageUrl} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a>
-          <><img src={user.imageUrl} alt={user.name}  style={{
-      width: user.imageSize,
-      height: user.imageSize
-    }} /></>
         </a>
       </div>
       <h1>Vite + React</h1>
