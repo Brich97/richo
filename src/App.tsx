1091c290624473a91362b4a84dbb3426ebb7f66e
Brittany R <brittanyrichards135@gmail.com>
2024-03-31 15:38:19 +1000

    Add background colour

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -10,7 +10,6 @@ const user =
   imageSize: 90,
 };
 
-
 function App() {
   const [count, setCount] = useState(0)
 
@@ -18,7 +17,7 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={user.imageUrl} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
@@ -36,6 +35,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      style={{
+        backgroundColor: 'pink',
+        width: '100px',
+        height: '100px'
+      }}
     </>
   )
 } //end
