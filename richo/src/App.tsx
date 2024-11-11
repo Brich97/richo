56e5314598b9dab20d7db4ac5a41627b062039f9
Brittany R <brittanyrichards135@gmail.com>
2024-11-11 22:20:34 +1000

    Update App.tsx

--- richo/src/App.tsx
+++ richo/src/App.tsx
@@ -28,6 +28,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="ball">
+      </div>
+      <p className="click-ball">
+        Click the ball
+      </p>
     </>
   )
 }
