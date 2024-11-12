3200e2b400c2d710ca79a1042a52fb7eb649bccb
Brittany R <brittanyrichards135@gmail.com>
2024-11-12 19:11:41 +1000

    add ball.gif

--- richo/src/App.tsx
+++ richo/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import ball from './assets/ball.gif'
 import './App.css'
 
 function App() {
@@ -29,6 +30,9 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <div className="ball">
+        <a>
+          <img src={ball} className="ball"></img>
+        </a>
       </div>
       <p className="click-ball">
         Click the ball
